b427c08af435cf92daf82eebabafca422d0d13ba
Alexander Kryuk <kryukalexander@gmail.com>
2017-04-23 12:29:29 +0600

    Added empty project at webpack

---
+++ project_webpack/postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    plugins: {
+        'postcss-import': {},
+        'postcss-cssnext': {
+            browsers: ['last 50 versions'],
+        },
+    },
+};
---
+++ project_webpack/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const styles = [
+    'css-loader',
+    'postcss-loader',
+    'sass-loader',
+];
+
+//todo insert babel
+
+module.exports = {
+    context: path.resolve(__dirname, './src'),
+    entry: './app.js',
+    output: {
+        path: path.resolve(__dirname, './dist/assets'),
+        filename: 'bundle.js',
+        publicPath: '/assets',
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, './src'),
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.(sass|scss)$/,
+                use: ExtractTextPlugin.extract({
+                    use: styles
+                })
+            },
+
+        ],
+    },
+    plugins: [
+    new ExtractTextPlugin('styles.css'),
+    ]
+};
