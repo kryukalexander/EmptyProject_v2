4b65656e56d451ad616a85df0e89b043d2e225f2
Alexander Kryuk <kryukalexander@gmail.com>
2017-08-08 12:41:35 +0600

    Autoprefixer and cssnano

--- project_gulp/gulpfile.js
+++ project_gulp/gulpfile.js
@@ -3,18 +3,29 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync');
-const watch = require('gulp-watch');
+const cssnano = require('gulp-cssnano');
+const autoprefixer = require('gulp-autoprefixer');
 
+//Setup
+const autoprefixerSettings = ['last 25 versions', '> 1%', 'ie 8', 'ie 7'];
+const root = 'src/';
+const dirs = {
+    scss: root + 'scss/**/*.scss',
+    css: root + 'css/',
+    html: root + 'templates/*.html'
+};
 
 //CSS
 gulp.task('dev:css', () => {
-    gulp.src('src/scss/*.scss')
+    gulp.src(dirs.scss)
     .pipe(sass.sync().on('error',  sass.logError))
-    .pipe(gulp.dest('./src/css/'));
+    .pipe(autoprefixer(autoprefixerSettings, {cascade: true}))
+    .pipe(cssnano())
+    .pipe(gulp.dest(dirs.css));
 });
 
 
-//browser sync
+//Browser sync
 gulp.task('browser-sync', () => {
     browserSync({
         server: {
@@ -28,8 +39,9 @@ gulp.task('browser-sync', () => {
     });
 });
 
+//Watch
 gulp.task('watch', ['dev:css', 'browser-sync'], () => {
-    gulp.watch('src/scss/**/*.scss', ['dev:css']);
-    gulp.watch('src/css/style.css', browserSync.reload);
-    gulp.watch('src/templates/*.html', browserSync.reload);
+    gulp.watch(dirs.scss, ['dev:css']);
+    gulp.watch(dirs.css + '*.css', browserSync.reload);
+    gulp.watch(dirs.html, browserSync.reload);
 });
