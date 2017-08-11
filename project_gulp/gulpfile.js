00661f6e44c77a33cbaa11c827b02e1675ac099a
Alexander Kryuk <kryukalexander@gmail.com>
2017-08-11 13:42:34 +0600

    Variables and js watcher in gulp project

--- project_gulp/gulpfile.js
+++ project_gulp/gulpfile.js
@@ -16,52 +16,72 @@ const del = require('del');
 //Setup
 const autoprefixerSettings = ['last 25 versions', '> 1%', 'ie 8', 'ie 7'];
 const root = 'src/';
+const build = 'build/';
+
 const dirs = {
-    scss: root + 'scss/**/*.scss',
-    css: root + 'css/',
+
+    scss: {
+        folder: root + 'scss/',
+        all: root + 'scss/**/*.scss',
+    },
+
+    css: {
+      dev:   root + 'css/',
+      build: build + 'css/'
+    },
+
+    html: {
+        root: root + 'templates/*.html',
+        all: root + 'templates/**/*.html',
+        dev: root + 'html/',
+        build: build + 'html/',
+    },
+
+    js: {
+        dev: root + 'js/*.js',
+        build: build + 'js'
+    },
+
     images: root + 'images/**/*',
     fonts: root + 'fonts/**/*',
-    html: root + 'templates/*.html',
-    htmlAll: root + 'templates/**/*.html',
-    htmlDev: root + 'templates_build',
+
+
     build: {
-        css: 'build/css/',
-        html: 'build/templates_build/',
-        images: 'build/images/',
-        fonts: 'build/fonts/'
+        images: build + 'images/',
+        fonts: build + 'fonts/'
     }
 };
 
 //CSS
 gulp.task('dev:css', () => {
-    gulp.src(dirs.scss)
+    gulp.src(dirs.scss.all)
         .pipe(sass.sync().on('error',  sass.logError))
         .pipe(autoprefixer(autoprefixerSettings, {cascade: true}))
-        .pipe(gulp.dest(dirs.css));
+        .pipe(gulp.dest(dirs.css.dev));
 });
 
 gulp.task('build:css', () => {
-    gulp.src(dirs.scss)
+    gulp.src(dirs.scss.all)
         .pipe(sass.sync().on('error',  sass.logError))
         .pipe(autoprefixer(autoprefixerSettings, {cascade: true}))
         .pipe(cssnano())
-        .pipe(gulp.dest(dirs.build.css));
+        .pipe(gulp.dest(dirs.css.build));
 });
 
 //HTML
 gulp.task('dev:html', () => {
-    gulp.src(dirs.html)
+    gulp.src(dirs.html.root)
         .pipe(preprocess())
-        .pipe(gulp.dest('src/templates_build/'));
+        .pipe(gulp.dest(dirs.html.dev));
 });
 
 gulp.task('build:html', () => {
-    gulp.src(dirs.html)
+    gulp.src(dirs.html.root)
         .pipe(preprocess())
         .pipe(useref())
         .pipe(gulpif('*.js', uglify()))
         .pipe(gulpif('*.css', cssnano()))
-        .pipe(gulp.dest(dirs.build.html));
+        .pipe(gulp.dest(dirs.html.build));
 });
 
 //Images
@@ -94,20 +114,20 @@ gulp.task('browser-sync', () => {
 
 //Watch
 gulp.task('watch', ['clean', 'dev:css', 'dev:html', 'browser-sync'], () => {
-    gulp.watch(dirs.scss, ['dev:css']);
-    gulp.watch(dirs.css + '*.css', browserSync.reload);
-    gulp.watch(dirs.htmlAll, ['dev:html', browserSync.reload]);
+    gulp.watch(dirs.scss.all, ['dev:css', browserSync.reload]);
+    gulp.watch(dirs.js.dev, browserSync.reload);
+    gulp.watch(dirs.html.all, ['dev:html', browserSync.reload]);
 });
 
 //Clean
 gulp.task('clean', () => {
-    del.sync('build/');
-    del.sync('src/css/');
-    del.sync('src/templates_build')
+    del.sync(build);
+    del.sync(dirs.css.dev);
+    del.sync(dirs.html.dev)
 });
 
 //Build
 gulp.task('build:all', ['clean', 'build:html', 'build:css', 'build:images'], () => {
     gulp.src(dirs.fonts).pipe(gulp.dest(dirs.build.fonts));
-    gulp.src('src/index.html').pipe(gulp.dest('build/'))
+    gulp.src('src/index.html').pipe(gulp.dest(build))
 });
