623b6debb3967565f06270f63e49328f37f6dead
Alexander Kryuk <kryukalexander@gmail.com>
2017-12-04 15:57:59 +0600

    Raster sprites WIP

--- project_webpack/postcss.config.js
+++ project_webpack/postcss.config.js
@@ -2,6 +2,41 @@ module.exports = {
     plugins: {
         'postcss-import': {},
         'autoprefixer': {},
-        'postcss-flexbugs-fixes' : {}
+        'postcss-flexbugs-fixes' : {},
+        'postcss-assets': {},
+        'postcss-sprites': {
+            spritePath: 'src/images/',
+            filterBy: (image) => {
+                if (!/\/images-sprite\//.test(image.url)) {
+                    return Promise.reject();
+                }
+                return Promise.resolve();
+            }
+        },
+        'cssnano' : {},
     },
 };
+
+
+// module.exports = {
+//     plugins: {
+//         'postcss-import': {},
+//         'autoprefixer': {},
+//         'postcss-flexbugs-fixes' : {},
+//         'postcss-assets': {},
+//         'postcss-at2x' : {
+//             skipMissingRetina: true
+//         },
+//         'postcss-sprites': {
+//             retina: true,
+//             spritePath: 'src/images/',
+//             filterBy: (image) => {
+//                 if (!/\/images-sprite\//.test(image.url)) {
+//                     return Promise.reject();
+//                 }
+//                 return Promise.resolve();
+//             }
+//         },
+//         'postcss-inline-svg': {},
+//     }
+// };
