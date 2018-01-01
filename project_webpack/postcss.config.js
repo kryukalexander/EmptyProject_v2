9e16094ab2ee763ef25892ccfab3b98ef1d6a4b5
Alexander Kryuk <kryukalexander@gmail.com>
2018-01-01 13:10:23 +0600

    at-2x plugin config fix. retina sprites disable by default

--- project_webpack/postcss.config.js
+++ project_webpack/postcss.config.js
@@ -4,12 +4,10 @@ module.exports = {
         'autoprefixer': {},
         'postcss-flexbugs-fixes' : {},
         'postcss-assets': {},
-        'postcss-at2x': {
-            skipMissingRetina: true
-        },
+        'postcss-at2x': {},
         'postcss-sprites': {
             spritePath: 'src/images/',
-            retina: true,
+            retina: false,
             filterBy: (image) => {
                 if (!/\/images-sprite\//.test(image.url)) {
                     return Promise.reject();
